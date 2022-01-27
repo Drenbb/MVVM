a7252592147b2c258d5345776ae64d2cd55f481e
Drenbb <Aa20021337>
2022-01-27 16:01:57 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    static class Model
+    {
+        public static int n = 0;
+        public static List<string> fam = new List<string>() { "Иванов", "Петров", "Сидоров" };
+    }
+}
---
+++ MVVM/ViewMpdel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    internal class ViewMpdel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string N
+        {
+            get
+            {
+                return Model.n.ToString();
+            }
+        }
+        public List<string> DataList { get; } = Model.fam;
+        int index = 0;
+        public int IndCh
+        {
+            set
+            {
+                index = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ListItem"));
+            }
+        }
+
+        public string ListItem
+        {
+            get
+            {
+                return DataList[index];
+            }
+
+        }
+
+        public RoutedCommand command { get; set; } = new RoutedCommand();
+        public CommandBinding binding;
+
+        public ViewMpdel()
+        {
+            binding = new CommandBinding(command);
+            binding.Executed += Binding_Executed;
+        }
+
+        private void Binding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Model.n++;
+            PropertyChanged(this, new PropertyChangedEventArgs("N"));
+        }
+    }
+}
